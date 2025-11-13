5bb8bded667ca321e6e525e1491d9a10b593b434
Iliana Dulina <ilyana.dulina07@gmail.com>
2025-11-13 18:04:44 +0200

    feature/app: Add visitor count functionality

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Tähän tulee WindFriends-sovellus!"
-
+    db = sqlite3.connect("database.db")
+    db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
+    db.commit()
+    result = db.execute("SELECT COUNT(*) FROM visits")
+    result = result.fetchone()
+    count = result[0]
+    db.close()
+    return "Sivua on ladattu " + str(count) + " kertaa"
